7df8b6f19204ea0245346b474cbb604092efa701
Marvel Collin <marvelcollin7@gmail.com>
2025-02-04 16:53:04 +0700

    feat: setuip

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use backend::db;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let pool = db::init_db().await?;
+    
+    println!("Database connected successfully!");
+    
+    tokio::signal::ctrl_c().await?;
+    Ok(())
 }
